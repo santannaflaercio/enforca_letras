e6f9ec8ac7cf19154e59970bcef45866a436afa9
Laércio Filho <santannaf.laercio@gmail.com>
2024-02-12 09:19:04 -0300

    improve main.py

--- src/main.py
+++ src/main.py
@@ -2,18 +2,21 @@ from utils import ler_palavras, mostra_jogo
 import random
 
 erros = 0
-acertos = 0
 letras_usadas = []
 palavras = ler_palavras()
 palavra_escolhida = random.choice(list(palavras)).lower()
 
-while erros < 6 and acertos < len(palavra_escolhida):
-
+while erros < 6 and len(set(palavra_escolhida) - set(letras_usadas)) > 0:
     # Mostra o estado atual do jogo
     mostra_jogo(palavra_escolhida, letras_usadas, erros)
 
     # Pede uma letra ao jogador
     letra = input("Digite uma letra: ").lower()
+
+    # Verifica se a entrada é válida
+    if not letra.isalpha() or len(letra) != 1:
+        print("Por favor, digite apenas uma única letra.")
+        continue
 
     # Verifica se a letra já foi usada
     if letra in letras_usadas:
@@ -25,8 +28,13 @@ while erros < 6 and acertos < len(palavra_escolhida):
 
     # Verifica se a letra está na palavra
     if letra in palavra_escolhida:
-        acertos += 1
         print("Acertou!")
     else:
         erros += 1
         print("Errou!")
+
+# Verifica se o usuário ganhou ou perdeu
+if erros >= 6:
+    print(f"Você perdeu! A palavra era '{palavra_escolhida}'.")
+else:
+    print("Parabéns, você ganhou!")
