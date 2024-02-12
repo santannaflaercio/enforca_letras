769c55bc2e424205b299f3c704e245ab12972571
Laércio Filho <santannaf.laercio@gmail.com>
2024-02-12 09:14:24 -0300

    add new tests

--- src/utils.py
+++ src/utils.py
@@ -1,31 +1,33 @@
 def ler_palavras():
+    # Inicializa um conjunto vazio para armazenar as palavras
+    palavras = set()
     try:
+        # Abre o arquivo de palavras no modo de leitura
         with open("data/palavras.txt", "r") as arquivo:
-            palavras = set(arquivo.readlines())
+            # Usa uma compreensão de conjunto para ler as palavras do arquivo
+            # e remover espaços em branco extras com o método strip()
+            palavras = {palavra.strip() for palavra in arquivo}
     except FileNotFoundError:
+        # Imprime uma mensagem de erro se o arquivo não for encontrado
         print("Erro: Arquivo 'palavras.txt' não encontrado.")
+        raise
     except Exception as e:
+        # Imprime uma mensagem de erro se ocorrer qualquer outra exceção ao ler o arquivo
         print(f"Erro ao ler o arquivo 'palavras.txt': {e}")
-    return palavras
+        raise
+    finally:
+        # Retorna o conjunto de palavras, mesmo que uma exceção tenha ocorrido
+        return palavras
 
 
 def mostra_jogo(palavra_escolhida, letras_usadas, erros):
-
     # Mostra a palavra com as letras acertadas
-    palavra_com_acertos = ""
-    for letra in palavra_escolhida:
-        if letra in letras_usadas:
-            palavra_com_acertos += letra
-        else:
-            palavra_com_acertos += "_"
+    palavra_com_acertos = "".join(letra if letra in letras_usadas else "_" for letra in palavra_escolhida)
 
     print("--------------------")
-
     # Mostra o número de erros
     print(f"Erros: {erros}")
-
     # Mostra a palavra com as letras acertadas
     print(f"Palavra: {palavra_com_acertos}")
-
     # Mostra as letras usadas
-    print(f"Letras usadas: {letras_usadas}")
+    print(f"Letras usadas: {' '.join(sorted(letras_usadas))}")
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,24 +1,56 @@
+import io
+import sys
 import unittest
-from unittest.mock import patch
-from src.utils import ler_palavras
+from unittest.mock import patch, mock_open
+from src.utils import ler_palavras, mostra_jogo
 
 
-class TestUtils(unittest.TestCase):
+class TestLerPalavras(unittest.TestCase):
+    def setUp(self):
+        self.capturedOutput = io.StringIO()
+        sys.stdout = self.capturedOutput
 
-    def test_ler_palavras_arquivo_valido(self):
-        # Simulando o conteúdo do arquivo
-        palavras_esperadas = ["Banana", "Maçã", "Laranja"]
+    def tearDown(self):
+        sys.stdout = sys.__stdout__
+        
+    @patch("builtins.open", new_callable=mock_open, read_data="palavra1\npalavra2\npalavra3")
+    def test_ler_palavras(self, mock_file):
+        expected_output = {"palavra1", "palavra2", "palavra3"}
+        self.assertEqual(ler_palavras(), expected_output)
 
-        # Simulando a abertura do arquivo
-        with patch("builtins.open") as mock_open:
-            mock_file = mock_open.return_value
-            mock_file.__enter__.return_value.readlines.return_value = palavras_esperadas
+    @patch("builtins.open", new_callable=mock_open, read_data="palavra1\npalavra2\npalavra2")
+    def test_ler_palavras_duplicadas(self, mock_file):
+        expected_output = {"palavra1", "palavra2"}
+        self.assertEqual(ler_palavras(), expected_output)
 
-            # Chamando a função a ser testada
-            palavras_lidas = ler_palavras()
+    @patch("builtins.open", side_effect=FileNotFoundError())
+    def test_ler_palavras_arquivo_nao_encontrado(self, mock_file):
+        ler_palavras()
+        output = self.capturedOutput.getvalue().strip()
+        self.assertIn("Erro: Arquivo 'palavras.txt' não encontrado.", output)
 
-            # Comparando os conjuntos
-            self.assertEqual(palavras_lidas, set(palavras_esperadas))
+
+class TestMostraJogo(unittest.TestCase):
+    def setUp(self):
+        self.capturedOutput = io.StringIO()
+        sys.stdout = self.capturedOutput
+
+    def tearDown(self):
+        sys.stdout = sys.__stdout__
+
+    def test_mostra_jogo(self):
+        mostra_jogo("python", set("yth"), 2)
+        output = self.capturedOutput.getvalue().strip()
+        expected_output = "--------------------\nErros: 2\nPalavra: _yth__\nLetras usadas: h t y"
+        self.assertEqual(output, expected_output)
+
+        self.capturedOutput.truncate(0)
+        self.capturedOutput.seek(0)
+
+        mostra_jogo("python", set("python"), 0)
+        output = self.capturedOutput.getvalue().strip()
+        expected_output = "--------------------\nErros: 0\nPalavra: python\nLetras usadas: h n o p t y"
+        self.assertEqual(output, expected_output)
 
 
 if __name__ == "__main__":
